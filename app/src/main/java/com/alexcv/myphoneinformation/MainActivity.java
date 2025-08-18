862d0fbcd05af627dbcde96ef555c25c8f3c8985
TrueAlexCv <alexcvtrue24282@gmail.com>
2025-08-18 13:59:04 +0200

    Added battery information

--- app/src/main/java/com/alexcv/myphoneinformation/MainActivity.java
+++ app/src/main/java/com/alexcv/myphoneinformation/MainActivity.java
@@ -1,6 +1,13 @@
 package com.alexcv.myphoneinformation;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_battery_temp;
+    private BroadcastReceiver batteryReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tv_battery_temp = findViewById(R.id.tv_battery_temp);
+
+        batteryReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int temp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+                float celsius = temp / 10f;
+                tv_battery_temp.setText("Temperatura: " + celsius + " °C");
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver(batteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(batteryReceiver);
     }
 }
